df4d357546d64f224d3c09a250b054fa098939a3
Bastien-Viatge <bastien.viatge@insa-lyon.fr>
2015-10-15 21:13:59 +0200

    re-log in

--- login.php
+++ login.php
@@ -6,4 +6,6 @@
 	<div id="login_pass">
 		<p>Password :  <input type="password" style="float:right;"></p>
 	</div>
+	<button>Log in</button><br/>
+	<p>You don't have an account ? <a href="">Sign in !</a></p>
 </div>
